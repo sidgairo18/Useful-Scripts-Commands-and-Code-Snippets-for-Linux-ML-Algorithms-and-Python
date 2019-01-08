b2a5039e6eda9df63f9a0a8d155ae0ad0b7f9ef1
sidgairo18 <siddhartha.gairola18@gmail.com>
2019-01-08 10:38:15 +0530

    Model created and initialzed

--- ML_Deep_Learning_Codes/Pytorch/Siamese_Network/train.py
+++ ML_Deep_Learning_Codes/Pytorch/Siamese_Network/train.py
@@ -55,6 +55,33 @@ def main():
     #testing_loader - Remember to update filenames_filename, triplet_filename
     train_loader = torch.utils.data.DataLoader(TripletImageLoader(base_path='.', filenames_filename='training_filename.txt', triplets_filename='training_triplet_filename.txt', transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])), batch_size = args.batch_size, shuffle=True, **kwargs)
 
+    # Defining CNN architecture
+
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+            self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+            self.conv2_drop = nn.Dropout2d()
+            self.fc1 = nn.Linear(320, 50)
+            self.fc2 = nn.Linear(50, 10)
+
+        def forward(self, x):
+            x = F.relu(F.max_pool2d(self.conv1(x), 2))
+            x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+            x = x.view(-1, 320)
+            x = F.relu(self.fc1(x))
+            x = F.dropout(x, training=self.training)
+            x = self.fc2(x)
+            return x
+
+    model = Net()
+    tnet = Tripletnet(model)
+    if args.cuda:
+        tnet.cuda()
+
+    print ("Model Initialized")
+
 
 ##################### Class for VisdomLinePlotter ##########################
 class VisdomLinePlotter(object):
